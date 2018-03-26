a05d16207bf09e7f66e0510d52152642f10d0a87
Charm <404373224@163.com>
2018-03-26 10:23:22 +0800

    后台服务端连接mssql数据库测试成功，访问超时时间暂未设置，经测试，默认60S。

--- Server/WebService/WebApp/views.py
+++ Server/WebService/WebApp/views.py
@@ -3,12 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 
+import sys
+sys.path.append('../')
+import Common.SqlServer as mssql
 
 def get_text_from_target(request):
+    sqlDBConnect = mssql.ODBCMS('{SQL SERVER}', r'192.168.1.254', 'test',
+                             'qiuguochao', 'qiuguochao')
+    sql = "select 1"
+    try:
+        sqlDBConnect.ExecQuery(sql)
+    except:
+        print("db connect error.")
+
     return render(
         request,
         'index.html',
-        {'get_text_from_target': request.GET.get('get_text_from_target', "")}
+        {'get_text_from_target': request.GET.get('get_text_from_target', sql)}
     )
 
 
